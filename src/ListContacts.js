3ad55f3cb048e9b7e9cbe0d77940b563f825bf0c
Courtney Noonan <courtneynoonan@me.com>
2017-07-27 13:22:38 -0400

    mapping the contacts from the contacts array into list items

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,6 +5,11 @@ class ListContacts extends React.Component {
   render() {
     return (
       <ol className='contact-list'>
+        {this.props.contacts.map( (contact) =>
+          <li key='contact.id'>
+            {contact.name}
+          </li>
+        )}
       </ol>
     )
   }
