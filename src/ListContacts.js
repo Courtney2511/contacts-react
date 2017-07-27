47c1192c0e352f136eb30f072b947f9a347ba7a1
Courtney Noonan <courtneynoonan@me.com>
2017-07-27 13:09:57 -0400

    starting ListContacts component

---
+++ src/ListContacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListContacts extends React.Component {
+  // render is the ONLY required property of a component class
+  render() {
+    return (
+      <ol className='contact-list'>
+      </ol>
+    )
+  }
+}
+
+export default ListContacts
