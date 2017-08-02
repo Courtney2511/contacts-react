afbbd60f761af5a275cc94f6dd659e3970399796
Courtney Noonan <courtneynoonan@me.com>
2017-08-02 14:56:25 -0400

    refactoring App to retrieve contacts from the contactsAPI

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+
+  // calls the ContactsAPI then sets the state of contacts to contacts
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
   }
 
   removeContact = (contact) => {
